fdef38469b7d6d6d8204fe49c705f96f11706d68
djdiptayan1 <111650788+djdiptayan1@users.noreply.github.com>
2022-10-08 23:23:02 +0530

    pull1_dj.c

---
+++ pull1_dj.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("ENTER FIRST NUMBER ");
+    scanf("%d", &a);
+
+    printf("ENTER SECOND NUMBER ");
+    scanf("%d", &b);
+
+    printf("SUM IS %d\n", a+b);
+
+    return 0;
+}
